ede4bc7091aa7aa15c61563e04e3a2235ecf878d
alulab14 <alulab14@inf.pucp.edu.pe>
2018-10-24 11:10:05 -0500

    es una prueba

--- Excellia/src/Vista/CalificarProyecto.java
+++ Excellia/src/Vista/CalificarProyecto.java
@@ -21,6 +21,7 @@ public class CalificarProyecto extends javax.swing.JDialog {
         initComponents();
         setLocationRelativeTo(parent);
         this.setTitle("Calificar Proyecto");
+        //kjcvn
     }
 
     /**
